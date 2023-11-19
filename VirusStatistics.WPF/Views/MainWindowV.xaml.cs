37f7f313185984cfef85c5e47e4a699a4db5b4de
Umnov Stanislav <umnov.msk@gmail.com>
2023-11-19 21:35:59 +0300

    Added MainWindow view and view model

--- VirusStatistics.WPF/Views/MainWindowV.xaml.cs
+++ VirusStatistics.WPF/Views/MainWindowV.xaml.cs
@@ -9,14 +9,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VirusStatistics.WPF
+namespace VirusStatistics.WPF.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowV : Window
     {
-        public MainWindow()
+        public MainWindowV()
         {
             InitializeComponent();
         }
