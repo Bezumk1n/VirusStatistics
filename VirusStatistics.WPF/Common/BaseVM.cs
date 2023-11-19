7d34125ea9acad54e2e0655855142c49f522717d
Umnov Stanislav <umnov.msk@gmail.com>
2023-11-19 21:35:30 +0300

    Added base view model with INotifyPropertyChanged implementation

---
+++ VirusStatistics.WPF/Common/BaseVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusStatistics.WPF.Common
+{
+    internal class BaseVM : INotifyPropertyChanged
+    {
+        #region Common properties
+        /// <summary>
+        /// Переопределяемы заголовок вью модели
+        /// </summary>
+        public virtual string Title
+        {
+            get => _Title;
+            set
+            {
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _Title = string.Empty;
+        #endregion
+        #region Notification
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> selectorExpression)
+        {
+            if (selectorExpression == null)
+                throw new ArgumentNullException("selectorExpression");
+            MemberExpression body = selectorExpression.Body as MemberExpression;
+
+            if (body == null)
+                throw new ArgumentException("The body must be a member expression");
+            OnPropertyChanged(body.Member.Name);
+        }
+        #endregion
+    }
+}
