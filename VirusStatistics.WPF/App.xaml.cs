bc9350ebac6cd15403b72f517cce68630297ddba
Umnov Stanislav <umnov.msk@gmail.com>
2023-11-19 21:33:46 +0300

    Added DI configuration

--- VirusStatistics.WPF/App.xaml.cs
+++ VirusStatistics.WPF/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
+using VirusStatistics.WPF.ViewModels;
+using VirusStatistics.WPF.Views;
 
 namespace VirusStatistics.WPF
 {
@@ -9,6 +11,37 @@ namespace VirusStatistics.WPF
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = CreateHostBuilder(new string[] { }).Build();
+        }
+        private IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args);
+            host.ConfigureServices((context, services) =>
+            {
+                // Конфигурация сервисов, добавляемых в DI
+
+            });
+            return host;
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            Window window = _host.Services.GetRequiredService<MainWindowV>();
+            window.DataContext = _host.Services.GetRequiredService<MainWindowVM>();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
+        }
     }
 
 }
